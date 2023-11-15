4fe2dfdc1bf4073a2c4cb30a4ff265d67a1efe98
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-11-15 19:21:25 -0300

    update

--- Ejercicios/Ejercicio_seis/Ejercicio_seis.py
+++ Ejercicios/Ejercicio_seis/Ejercicio_seis.py
@@ -1,13 +1,14 @@
 class Avion():
 
-    def __init__(self):
-        pass
+    def __init__(self, avion: int, aviones_estacionados: int):
+        self.avion = avion
+        self.aviones_estacionados = aviones_estacionados
 
     def __str__(self):
-        pass
+        return f"Cantidad de aviones en un aeropuerto {self.avion}, cantidad de aviones estacionados: {self.aviones_estacionados}"
 
     def __repr__(self):
-        pass
+        return f"{self.avion}, {self.aviones_estacionados}"
 
 
 class Aeropuerto():
