77e6b6297d9a4bc1b8ae89567264b3e8df0f4a1e
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-11-15 19:19:17 -0300

    adding

---
+++ Ejercicios/Ejercicio_seis/Ejercicio_seis.py
@@ -0,0 +1,22 @@
+class Avion():
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+class Aeropuerto():
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
