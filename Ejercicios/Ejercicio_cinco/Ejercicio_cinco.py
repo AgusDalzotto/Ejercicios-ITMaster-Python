3c9c7c6f2a404a27d23e79164edd9b6292a109f8
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-11-15 19:08:41 -0300

    adding

---
+++ Ejercicios/Ejercicio_cinco/Ejercicio_cinco.py
@@ -0,0 +1,22 @@
+class Instrumento():
+
+    def __init__(self, instrumento: int):
+        self.instrumento = instrumento
+
+    def __str__(self) -> str:
+        return f""
+
+    def __repr__(self) -> str:
+        pass
+
+
+class Orquesta():
+
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+    def __repr__(self) -> str:
+        pass
